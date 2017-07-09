5a7f1f041895ab562b79e18e8806da716ca6ebb2
Anubhav Tiwari <atworkstudios@gmail.com>
2017-07-09 14:29:01 +0530

    text_edit

--- auto_tag_closer/tag_closer.js
+++ auto_tag_closer/tag_closer.js
@@ -1,9 +1,24 @@
 const area = document.querySelector('#text'),
 stack = [],
-tags = ['html','head','title','div','header','p',
-'a','body','textarea','form'];
+tags = [{ name : 'html', auto_close : false },
+		{ name : 'head', auto_close : false },
+		{ name : 'title', auto_close : false },
+		{ name : 'div', auto_close : false },
+		{ name : 'header', auto_close : false },
+		{ name : 'p', auto_close : false },
+		{ name : 'a', auto_close : false },
+		{ name : 'body', auto_close : false },
+		{ name : 'textarea', auto_close : false },
+		{ name : 'form', auto_close : false },
+		{ name : 'script', auto_close : false },
+		{ name : 'style', auto_close : false },
+		{ name : 'input', auto_close : true },
+		{ name : 'br', auto_close : true },
+		{ name : 'meta', auto_close : true },
+		{ name : 'link', auto_close : true }]
 
-let s_div = document.getElementById('s'),
+
+let s_div = document.getElementById('u'),
 suggestions = [];
 
 class tag{
@@ -26,6 +41,10 @@ area.addEventListener('keypress',e=>{
 		return;
 	}
 
+	if(e.keyCode === 8){
+		console.log('you deleted something');
+	}
+
 	if(checkState && e.keyCode === 47){
 		e.preventDefault();
 		area.value += `/${stack.pop().name}>`;
@@ -40,24 +59,51 @@ area.addEventListener('keypress',e=>{
 
 	if(e.keyCode === 62 && !stack[stack.length-1].gotName){
 		stack[stack.length-1].gotName = true;
+		let [found,val] = search(stack[stack.length-1],true);
+		if(found){
+			e.preventDefault();
+			area.value += (e.key+val);
+		}
 	}
 
 	else if(!stack[stack.length-1].gotName){
 		stack[stack.length-1].name += e.key;
-		search(stack[stack.length-1].name);
+		if(e.keyCode === 32){
+			stack[stack.length-1].gotName = true;
+			stack[stack.length-1].name = stack[stack.length-1].name.trim();
+		}
+		let [found,val] = search(stack[stack.length-1]);
+		if(found){
+			e.preventDefault();
+			area.value += (e.key+val);
+		}
 	}
 	console.log(e.keyCode);
 })
 
-function search(key){
+function search(key,closed=false){
 	console.log('Searching');
 	suggestions = [];
+	let found = false,
+	val = '';
 	for(let i of tags){
-		if(i.includes(key)){
-			suggestions.push(i);			
+		if(i.name.includes(key.name)){
+			suggestions.push(i.name);			
+		}
+		if(i.name === key.name){
+			found = true;
+			if(i.auto_close===false && closed=== true){
+				stack.pop();
+				val = `</${key.name}>`; 
+			}
+			else if(i.auto_close){
+				stack.pop();
+				val = ' />';
+			}  
 		}
 	}
 	show_suggestions(suggestions);
+	return [found,val];
 }
 
 function show_suggestions(s){
